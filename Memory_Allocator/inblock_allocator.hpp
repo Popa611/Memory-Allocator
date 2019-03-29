c4b0b32a5a24e33532a7f7213d4674eff4a6f198
ThePopa611 <wowinec@gmail.com>
2019-03-29 10:24:39 +0100

    Doubly linked free list allocation

--- Memory_Allocator/inblock_allocator.hpp
+++ Memory_Allocator/inblock_allocator.hpp
@@ -1,7 +1,27 @@
+/*
+	REQUIRED STD:C++17 for inline static members (so that the user does not have to define those static members in .cpp file
+													and so that this header can be included multiple times)
+
+
+
+*/
 #ifndef inblock_allocator_hpp
 #define inblock_allocator_hpp
 
 #include <cstdint>
+
+#include<iostream>
+
+// Acts as a header before actual payload
+struct Block
+{
+	std::size_t size;
+	Block* next;
+	Block* previous;
+};
+
+// We align returned addresses to word - 8 bytes
+using word = uint64_t;
 
 class inblock_allocator_heap 
 {
@@ -9,19 +29,25 @@ class inblock_allocator_heap
 	friend class inblock_allocator;
 
 private:
-	static  std::size_t n_bytes_;
-	static void* ptr_;
+	inline static  std::size_t n_bytes_;	// Heap size in bytes
+	inline static Block* ptr_;	// Start of the heap
+	inline static Block* head;	// Head of the free list
 
 public:
 	void operator()(void* ptr, std::size_t n_bytes) 
 	{  
 		n_bytes_ = n_bytes;
-		ptr_ = ptr;
+		ptr_ = (Block*)ptr;
+		ptr_->next = nullptr;
+		ptr_->previous = nullptr;
+		ptr_->size = n_bytes - sizeof(Block);	// size indicates only bytes free for data (without header overhead)
+		head = ptr_;
 	}
 };
 
-std::size_t inblock_allocator_heap::n_bytes_ = 0;
-void* inblock_allocator_heap::ptr_ = nullptr;
+/*std::size_t inblock_allocator_heap::n_bytes_ = 0;
+Block* inblock_allocator_heap::ptr_ = nullptr;
+Block* inblock_allocator_heap::head = nullptr;*/
 
 template<typename T, typename HeapHolder>
 class inblock_allocator 
@@ -31,7 +57,14 @@ public:
 	
 	T* allocate(std::size_t n)
 	{
-		return nullptr;
+		std::size_t size = get_aligned_size(n*sizeof(T));	// Needed size for user data
+		Block* best = find_best_fit(size+sizeof(Block));	// Find the best-fit block (which can accommodate both payload and header
+
+		remove_and_split(best, size);
+
+		best->size = size;
+
+		return (T*)((char*)best + sizeof(Block));	// We return header+sizeof(Block) -> pointer to memory for user data
 	}
 
 	void deallocate(T* ptr, std::size_t n)
@@ -41,12 +74,96 @@ public:
 
 	inblock_allocator() = default;
 	template <typename U, typename Holder> 
-	constexpr inblock_allocator(const inblock_allocator<U, Holder>&) noexcept {}
+	constexpr inblock_allocator(const inblock_allocator<U, Holder>&) noexcept { }
 
 	template <typename U, typename Holder>
 	bool operator==(const inblock_allocator<U,Holder>&) { return true; }
 	template <typename U, typename Holder>
 	bool operator!=(const inblock_allocator<U, Holder>&) { return false; }
+
+private:
+	std::size_t get_aligned_size(std::size_t size)
+	{
+		return (size + sizeof(word) - 1) & ~(sizeof(word) - 1);
+	}
+
+	// Returns the minimum sized block, which can accommodate the data
+	Block* find_best_fit(std::size_t size)
+	{
+		Block* it = HeapHolder::heap.head;
+		Block* best = it;
+
+		while (it)
+		{
+			if (it->size >= size && it->size < best->size)
+			{
+				best = it;
+			}
+			it = it->next;
+		}
+		return best;
+	}
+
+	// Removes block from the free list and adds remainder if necessary
+	void remove_and_split(Block* block, std::size_t needed_size)
+	{
+		// If the block size is exactly what we need -> simply remove
+		if (block->size == needed_size)
+		{
+			// It's somewhere in middle
+			if (block->previous && block->next)
+			{
+				block->previous->next = block->next;
+				block->next->previous = block->previous;
+			}
+			else if (block->previous)	// It's at the end
+			{
+				block->previous->next = nullptr;
+			}
+			else if (block->next)	// At the beginning
+			{
+				HeapHolder::heap.head = block->next;
+				block->next->previous = nullptr;
+			}
+			else
+			{
+				// nothing to do (the free list remains empty because we have nothing to add)
+			}
+		}
+		else	// If the block is bigger, we split it and add a new chunk to free list
+		{
+			Block* remainder = (Block*)((char*)block + needed_size + sizeof(Block));
+			remainder->size = block->size - needed_size - sizeof(Block);
+
+			if (block->previous && block->next)
+			{
+				block->previous->next = remainder;
+				block->next->previous = remainder;
+
+				remainder->next = block->next;
+				remainder->previous = block->previous;
+			}
+			else if (block->previous)
+			{
+				block->previous->next = remainder;
+				remainder->previous = block->previous;
+				remainder->next = nullptr;
+			}
+			else if (block->next)
+			{
+				HeapHolder::heap.head = remainder;
+				block->next->previous = remainder;
+				remainder->next = block->next;
+				remainder->previous = nullptr;
+			}
+			else
+			{
+				remainder->next = nullptr;
+				remainder->previous = nullptr;
+				HeapHolder::heap.head = remainder;
+			}
+		}
+	}
 };
 
 #endif
